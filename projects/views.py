70e2122614a49c429592163f061f70d02409544b
Limo <liam@acordsoftware.tech>
2020-07-23 13:57:04 +0800

    #1 - Project App: Views (Part 1)
    Clickup: CU-20uw7k; https://share.clickup.com/t/h/20uw7k/NAB63ZCYJLQIERA
    
    To be continued!
    
    Also will do Unity and this tomorrow (unity tutorial from zenva https://github.com/gizmotronn/unity-intro/issues/8)
    
    Co-Authored-By: Liam Arbuckle <allianceofdroidsla@gmail.com>

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from projects.models import project
 
-# Create your views here.
+def project_index(request):
+  projects = Projects.objects.all()
+  context = {
+    'projects': projects
+  }
+  return render(request, 'project_index.html`, context)
