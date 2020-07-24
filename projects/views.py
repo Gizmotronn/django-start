a3a9d4962208e0ce08d30b679c9f1b43ce7d9589
Limo <liam@acordsoftware.tech>
2020-07-24 09:29:35 +0800

    #1 Django - Views finish + set up URLs
    `Project_details function` for Views
    
    Set up URLs for Project_details & project_index
    
    Clickup: CU-20uw7k
    Co-Authored-By: Liam Arbuckle <allianceofdroidsla@gmail.com>

--- projects/views.py
+++ projects/views.py
@@ -7,3 +7,10 @@ def project_index(request):
     'projects': projects
   }
   return render(request, 'project_index.html`, context)
+
+def project_detail(request, pk):
+  project = Project.objects.get(pk=pk)
+  context = {
+    'project': project
+  }
+  return render(request, 'project_detail.html', context)
