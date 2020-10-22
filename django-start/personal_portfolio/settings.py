f773a8d5d28611cf96dbeda738b69f7c002cb304
LArbuckle <liam@acordsoftware.tech>
2020-10-22 15:54:57 +0800

    #1 - DIRS for personal_portfolio & hello_world
    Co-Authored-By: Liam Arbuckle <allianceofdroidsla@gmail.com>

--- django-start/personal_portfolio/settings.py
+++ django-start/personal_portfolio/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'personal_portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": ["personal_portfolio/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
