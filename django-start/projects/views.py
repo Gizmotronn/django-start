8aadc88b1f3a5ecbd6d01cb5cfa44be2d0a8dd96
Limo <liam@acordsoftware.tech>
2020-10-27 12:40:21 +0800

    #1 - Projects App: Views & Set up projects with django shell
    Co-Authored-By: Liam Arbuckle <allianceofdroidsla@gmail.com>

--- django-start/projects/views.py
+++ django-start/projects/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from projects.models import Project
 
-# Create your views here.
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)
