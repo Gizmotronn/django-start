f894080d7682c9ddf5cedff4f236e2fcfedba6a1
Liam Arbuckle <droidology@allianceofdroids.org.au>
2020-10-29 09:13:07 +0800

    #1 - Projects app - `project_details()` function
    @IrisDroidology

--- django-start/projects/views.py
+++ django-start/projects/views.py
@@ -7,3 +7,10 @@ def project_index(request): # define the project_index class (using request as a
         'projects': projects 
     }
     return render(request, 'project_index.html', context) # In line 9, context is added as an argument to render(). Any entries in the context dictionary are available in the template, as long as the context argument is passed to render(). You’ll need to create a context dictionary and pass it to render in each view function you create.
+
+def project_detail(request, pk): # define the `project_detail` function with the `pk` (id) and request parameters for the project [object/orm]
+  project = Project.objects.get(pk=pk) # set what the `project` is - gathering the objects of `Project`. /#/ In line 14, we perform another query. This query retrieves the project with primary key, pk, equal to that in the function argument. We then assign that project in our context dictionary, which we pass to render(). Again, there’s a template project_detail.html, which we have yet to create.
+  context = { # set the context - see above variable
+    'project': project
+  }
+  return render(request, 'project_details.html', context)
