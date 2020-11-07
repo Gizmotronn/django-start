7b2e1fa131a9d48615e26ce133403f6a6363be57
Limo <liam@acordsoftware.tech>
2020-11-07 09:17:06 +0800

    #1 - blog_index view
    Co-Authored-By: Liam Arbuckle <allianceofdroidsla@gmail.com>

--- django-start/blog/views.py
+++ django-start/blog/views.py
@@ -1,37 +1,18 @@
 from django.shortcuts import render
-
-from blog.forms import CommentForm
 from blog.models import Post, Comment
 
-
 def blog_index(request):
-    posts = Post.objects.all().order_by("-created_on")
-    context = {"posts": posts}
+	posts = Post.objects.all().order_by('-created_on')
+    context = {
+        "posts": posts,
+    }
     return render(request, "blog_index.html", context)
 
 
-def blog_category(request, category):
-    posts = Post.objects.filter(categories__name__contains=category).order_by(
-        "-created_on"
-    )
-    context = {"category": category, "posts": posts}
-    return render(request, "blog_category.html", context)
+""" blog_index view
+`line 2` → import the `Post` & `Comment` modules (aka models) from the blog models
 
+`line 5` → obtain a queryset containing all posts in the database. `order_by` orders the Queryset according to the **argument** (the date the post was created) given. The `-` tells Django to start with the **largest** value. 
 
-def blog_detail(request, pk):
-    post = Post.objects.get(pk=pk)
-    comments = Comment.objects.filter(post=post)
-
-    form = CommentForm()
-    if request.method == "POST":
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = Comment(
-                author=form.cleaned_data["author"],
-                body=form.cleaned_data["body"],
-                post=post,
-            )
-            comment.save()
-
-    context = {"post": post, "comments": comments, "form": form}
-    return render(request, "blog_detail.html", context)
+`line 6` → define the context dictionary and render the template `blog_index.html`
+"""
