884ec5d693350e276b11f7f57a8e693ebe98e08c
Liam Arbuckle <droidology@allianceofdroids.org.au>
2020-12-20 19:02:17 +0000

    #5 create core/models.py classes for schoolCourses

--- school/core/admin.py
+++ school/core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Person, Course, Grade # import 3 classes from the file core/models.py
+
+@admin.register(Person) # register the Person model
+class PersonAdmin(admin.ModelAdmin): # we create a class called PersonAdmin that inherits from admin.ModelAdmin
+    pass # stub
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
+    pass
--- school/core/models.py
+++ school/core/models.py
@@ -1,3 +1,23 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+class Person(models.Model): # Create a model called "Person" that inherits from models.Model class
+    last_name = models.TextField() # Three attributes - first name, last name, and courses (which is a foreign key relating to the Course class/model)
+    first_name = models.TextField()
+    courses = models.ManyToManyField("Course", blank=True)
+
+    class Meta:
+        verbose_name_plural = "People"
+
+class Course(models.Model): 
+    name = models.TextField()
+    year = models.IntegerField()
+
+    class Meta:
+        unique_together = ("name", "year", )
+
+class Grade(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE) # make a grade attribute for each course in each student
+    grade = models.PositiveSmallIntegerField( # the student's grade in a course can be a value from 1-100
+        validators=[MinValueValidator(0), MaxValueValidator(100)])
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
