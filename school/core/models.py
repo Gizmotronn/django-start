202c0bf6edfd5cce21c0b769a8adf6fbd39fff63
Liam Arbuckle <droidology@allianceofdroids.org.au>
2020-12-21 17:41:19 +0000

    #5 Create methods to display model data as plain text

--- school/core/models.py
+++ school/core/models.py
@@ -6,18 +6,21 @@ class Person(models.Model): # Create a model called "Person" that inherits from 
     first_name = models.TextField()
     courses = models.ManyToManyField("Course", blank=True)
 
-    class Meta:
-        verbose_name_plural = "People"
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}"
 
 class Course(models.Model): 
     name = models.TextField()
     year = models.IntegerField()
 
-    class Meta:
-        unique_together = ("name", "year", )
+    def __str__(self):
+        return f"{self.name}, {self.year}"
 
 class Grade(models.Model):
     person = models.ForeignKey(Person, on_delete=models.CASCADE) # make a grade attribute for each course in each student
     grade = models.PositiveSmallIntegerField( # the student's grade in a course can be a value from 1-100
         validators=[MinValueValidator(0), MaxValueValidator(100)])
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.grade}, {self.person}, {self.course}"
