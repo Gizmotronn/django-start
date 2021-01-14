66be9ef2eb57e42c8475dc17ed038e776d7ea0e9
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-01-14 03:30:47 +0000

    #6 Got a problem with app.py

--- avocado_analytics/app.py
+++ avocado_analytics/app.py
@@ -12,3 +12,43 @@ data.sort_values("Date", inplace=True) # sort by date
 
 # Create instance of the Dash Class
 app = dash.Dash(__name__)
+
+# Create the layout property of the Dash app
+app.layout """define parent component - an html div element"""= html.Div( # Using dash_html_components library 
+    children=[ # add a child element to the html div
+        html.H1(children="Avocado Analytics",), # first child element is a heading - h1
+        html.P( # secondchild element is a paragraph - p
+            children="Analyze the behavior of avocado prices"
+            " and the number of avocados sold in the US"
+            " between 2015 and 2018",
+        ),
+        dcc.Graph( # dash core components/graph. /#/ This one is plotting the average price during period of study
+            figure={
+                "data": [
+                    {
+                        "x": data["Date"],
+                        "y": data["AveragePrice"],
+                        "type": "lines",
+                    },
+                ],
+                "layout": {"title": "Average Price of Avocados"},
+            },
+        ),
+        dcc.Graph( # graph plotting number of aUnits sold
+            figure={
+                "data": [
+                    {
+                        "x": data["Date"],
+                        "y": data["Total Volume"],
+                        "type": "lines",
+                    },
+                ],
+                "layout": {"title": "Avocados Sold"},
+            },
+        ),
+    ]
+)
+
+# Run the app
+if __name__ == "__main__": # run app locally using flask's built-in server
+    app.run_server(debug=True"""Enables hot reload""")
---
+++ lookup.py
@@ -0,0 +1,36 @@
+"""This is so that when I'm copying snippets I don't have to keep switching between tabs, I can just have two files open in codespaces"""
+
+app.layout = html.Div(
+    children=[
+        html.H1(children="Avocado Analytics",),
+        html.P(
+            children="Analyze the behavior of avocado prices"
+            " and the number of avocados sold in the US"
+            " between 2015 and 2018",
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": data["Date"],
+                        "y": data["AveragePrice"],
+                        "type": "lines",
+                    },
+                ],
+                "layout": {"title": "Average Price of Avocados"},
+            },
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": data["Date"],
+                        "y": data["Total Volume"],
+                        "type": "lines",
+                    },
+                ],
+                "layout": {"title": "Avocados Sold"},
+            },
+        ),
+    ]
+)
